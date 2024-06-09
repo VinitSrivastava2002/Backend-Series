7066d3ec8dd4266a65a538562fb7d1423ed26811
Vinit Srivastava <vinitsss1234@gmail.com>
2024-06-09 16:24:54 +0530

    Add user and vedio model

---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const newSchema = new mongoose.Schema(
+  {
+    videoFile: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      // cloudinary url
+      type: true,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      // cloudinary
+      type: Number,
+    },
+    views: {
+      type: Numbers,
+      default: 0,~
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  { timestamps: true }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", newSchema)
