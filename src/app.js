d913946a5047e778e83d60f846bad4c5fa828302
Vinit Srivastava <vinitsss1234@gmail.com>
2024-06-11 16:56:55 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -16,4 +16,9 @@ app.use(express.urlencoded({ extented: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
 export { app };
